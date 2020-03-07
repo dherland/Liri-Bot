b477b09cabb0ae0e4e5b7ea8023e8c0147f46e91
bluegloom <dherland99@gmail.com>
2020-03-06 23:46:22 -0800

    code

--- liri.js
+++ liri.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 var axios = require("axios");
+var moment = require("moment");
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+function concertThis(artist){
 
-"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    axios.get(url)
+        .then(function(res){
+            console.log(res);
+            
+        })
+}
